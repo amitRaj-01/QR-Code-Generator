78129913086c530e609a08032508b0c1af526ead
Amit Raj <amitkraj20@gmail.com>
2025-10-26 11:56:35 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let API = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+
+let qrText = document.getElementById("qrText");
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let genBtn = document.getElementById("genBtn");
+
+async function generateQR(inputText) {
+    let textLength = inputText.length;
+    if (textLength > 0) {
+        qrImage.src = `${API}${encodeURIComponent(inputText)}`; 
+        imgBox.classList.add("show-img");
+    } else {
+        qrText.classList.add("error");
+        setTimeout(() => {
+            qrText.classList.remove("error");
+        }, 1000);
+    }
+}
+
+genBtn.addEventListener("click", () => {
+    let inputText = qrText.value;
+    generateQR(inputText);
+});
